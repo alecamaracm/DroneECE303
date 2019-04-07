d6aceff5cd0083c6423d58305c93b329518318d7
alecamaracm <alecamar@hotmail.es>
2019-04-06 23:55:18 -0400

    PWM and initial IMU, Heigth and BME280

---
+++ MainCode/sketch_apr02a/pinDefinitions.cpp
@@ -0,0 +1,10 @@
+#define PIN_BUZZER 4
+
+#define PIN_PWM_BLANK 18
+#define PIN_PWM_SCLK 19
+#define PIN_PWM_LATCH 22
+#define PIN_PWM_SIN 21
+#define PIN_PWM_GSCLK 23
+
+#define PIN_I2C_SDA 14
+#define PIN_I2C_SCL 27
--- MainCode/sketch_apr02a/sketch_apr02a.ino
+++ MainCode/sketch_apr02a/sketch_apr02a.ino
@@ -1,9 +1,21 @@
-#include <Wire.h>
-#define PIN_BUZZER 4
+#include "pinDefinitions.cpp"
 
-boolean ledState = false;
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <Adafruit_BME280.h>
+
+#include <VL53L0X.h>
+//#include "Adafruit_VL53L0X.h"
+
+//Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+
+int PWM_freq=50;
+
+boolean ledState = false; 
 byte data_buf[16];
 
+int pwmData[16];
 
 int IRx[4];
 int IRy[4];
@@ -12,27 +24,204 @@ int IRbuff;
 int IRv1[4]; //X1,Y1,X2,Y2
 bool IRDataFine=true;
 
+bool newPWMDataToOutput=false;
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
 int IrTLeft,IrTRight,IrTBack,IrTMiddle;
 
+hw_timer_t * PWMtimer = NULL;
+
+Adafruit_BME280 bme;
+Adafruit_BNO055 bno = Adafruit_BNO055();
+
+
+VL53L0X sensor;
 
 void setup() {
 Serial.begin(115200);
 
   //  initializeIRSensor();
-
+  startPWM();   
   pinMode(PIN_BUZZER,OUTPUT);
+
+  /* Initialise the sensor */
+  if(!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+  if (!bme.begin()) {
+      Serial.println("Could not find a valid BME280 sensor, check wiring!");
+      while (1);
+  }
+
+/*  if (!lox.begin()) {
+    Serial.println(F("Failed to boot VL53L0X"));
+    while(1);
+  }*/
+
+    Wire.begin(14,27);
+
+  sensor.init();
+  sensor.setTimeout(500);
+
+sensor.setSignalRateLimit(0.1);
+  // increase laser pulse periods (defaults are 14 and 10 PCLKs)
+  sensor.setVcselPulsePeriod(VL53L0X::VcselPeriodPreRange, 18);
+  sensor.setVcselPulsePeriod(VL53L0X::VcselPeriodFinalRange, 14);
+  
+
+  delay(3000);
+
+
+  bno.setExtCrystalUse(true);
+
+
 }
 
 void loop() {
-  Serial.println("hi");
+
+
+  if(Serial.available()>0)
+  {
+    int pwr=Serial.parseInt();
+    setMotorPower(1,pwr,true);    
+    setMotorPower(2,pwr,true);    
+    setMotorPower(3,pwr,true);    
+    setMotorPower(4,pwr,true);    
+    Serial.print("Setting power to: ");
+    Serial.println(pwr);
+  }
+
   
+  Serial.println("Calibration status values: 0=uncalibrated, 3=fully calibrated");
+  uint8_t system, gyro, accel, mag = 0;
+  bno.getCalibration(&system, &gyro, &accel, &mag);
+  Serial.print("CALIBRATION: Sys=");
+  Serial.print(system, DEC);
+  Serial.print(" Gyro=");
+  Serial.print(gyro, DEC);
+  Serial.print(" Accel=");
+  Serial.print(accel, DEC);
+  Serial.print(" Mag=");
+  Serial.println(mag, DEC);
+
+  delay(1000);
+
+
+
+  Serial.print("Temperature = ");
+    Serial.print(bme.readTemperature());
+    Serial.println(" *C");
+
+    Serial.print("Pressure = ");
+
+    Serial.print(bme.readPressure() / 100.0F);
+    Serial.println(" hPa");
+
+    Serial.print("Approx. Altitude = ");
+    Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+    Serial.println(" m");
+
+    Serial.print("Humidity = ");
+    Serial.print(bme.readHumidity());
+    Serial.println(" %");
+
+    Serial.println();
+
+  /* Display the current temperature */
+  int8_t temp = bno.getTemp();
+  Serial.print("Current Temperature: ");
+  Serial.print(temp);
+  Serial.println(" C");
+  Serial.println("");
+
+  /*VL53L0X_RangingMeasurementData_t measure;
+    
+  Serial.print("Reading a measurement... ");
+  lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
+
+  if (measure.RangeStatus != 4) {  // phase failures have incorrect data
+    Serial.print("Distance (mm): "); Serial.println(measure.RangeMilliMeter);
+  } else {
+    Serial.println(" out of range ");
+  }*/
+ Serial.print(sensor.readRangeSingleMillimeters());
+  if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+
+  Serial.println();
 /*  updateRawIRData();
   processIRData();
   printIRPoints();
   printIRData();*/
-
 }
 
+void setMotorPower(int motorID,float power,bool sendUpdate)
+{
+  if(motorID>15) return;
+  
+  pwmData[motorID-1]=4096-(205+((power/100)*205));
+  //pwmData[motorID]=(power/100)*4095;
+  
+  if(sendUpdate)
+  {
+    sendPWMData();
+  }
+}
+
+
+void startPWM()
+{
+  PWMtimer = timerBegin(1, 10, true);  //8,000,000
+  timerAttachInterrupt(PWMtimer, &onPWMTimer, true);
+  timerAlarmWrite(PWMtimer, 8000000/PWM_freq, true);
+  timerAlarmEnable(PWMtimer);
+
+  ledcSetup(0, PWM_freq*4096, 1); //This sets up the main clock for the PWM IC (TCL5940)
+  ledcWrite(0,1);
+  ledcAttachPin(PIN_PWM_GSCLK, 0);
+
+  for(int i=0;i<16;i++)  pwmData[i]=4096-(205);
+
+  pinMode(PIN_PWM_BLANK,OUTPUT);
+  pinMode(PIN_PWM_SCLK,OUTPUT);
+  pinMode(PIN_PWM_LATCH,OUTPUT);
+  pinMode(PIN_PWM_SIN,OUTPUT);
+  pinMode(PIN_PWM_BLANK,OUTPUT);
+  
+  digitalWrite(PIN_PWM_BLANK,HIGH); //HIGH means off
+  digitalWrite(PIN_PWM_SCLK,LOW);
+  digitalWrite(PIN_PWM_LATCH,LOW);  //HIGH pulse of at least 20ns after sending the data to activate it 
+  digitalWrite(PIN_PWM_SIN,LOW);
+}
+
+void onPWMTimer()
+{
+  digitalWrite(PIN_PWM_BLANK,HIGH); //Stops the PWM outputs
+  if(newPWMDataToOutput)
+  {
+    digitalWrite(PIN_PWM_LATCH,HIGH);
+    digitalWrite(PIN_PWM_LATCH,LOW);
+    newPWMDataToOutput=false;
+  } 
+  digitalWrite(PIN_PWM_BLANK,LOW);
+}
+
+void sendPWMData()
+{
+   for(int i=15;i>=0;i--) shift12BitsOut(PIN_PWM_SIN,PIN_PWM_SCLK, pwmData[i]);
+   newPWMDataToOutput=true;
+}
+
+void shift12BitsOut(uint8_t dataPin, uint8_t clockPin, int val) {
+    for(uint8_t i = 0; i < 12; i++) {
+        digitalWrite(dataPin, !!(val & (1 << (11 - i))));
+        digitalWrite(clockPin, HIGH);
+        digitalWrite(clockPin, LOW);  
+    }
+}
 
 
 
