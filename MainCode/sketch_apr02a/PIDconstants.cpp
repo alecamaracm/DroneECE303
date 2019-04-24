ac52e2572f3b1f33f32e7f0110d168ee9e44d9f3
Alejandro Cabrerizo <alecamar@hotmail.es>
2019-04-24 12:58:01 -0400

    PID constants update

--- MainCode/sketch_apr02a/PIDconstants.cpp
+++ MainCode/sketch_apr02a/PIDconstants.cpp
@@ -1,12 +1,10 @@
 #define XBOX_KP 0.20f
 #define XBOX_KD 0.050f
 #define XBOX_KI 0.005f
-#define XBOX_KP_YAW 0.05f
+#define XBOX_KP_YAW 0.0f
 #define XBOX_KD_YAW 0.0f
 #define XBOX_KI_YAW 0.0f
 
 #define XBOX_PITCH_MAX_CHANGE 25
 #define XBOX_ROLL_MAX_CHANGE 25
 #define XBOX_YAW_MAX_CHANGE 10
-
-
